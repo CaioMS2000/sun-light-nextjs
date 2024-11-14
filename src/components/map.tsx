e49c21bcf7d96e7bc7d06c4a1ec25f94c07c4a8c
Caio <caiocpe@gmail.com>
2024-11-13 22:41:03 -0300

    tutorial about a newer Google Map lib

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,56 +1,110 @@
 'use client'
 import { z } from 'zod'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
-
-const containerStyle = {
-  width: '100%',
-  height: '100%',
-}
 
 const center = {
-  lat: -16.715799249062005,
-  lng: -49.2982311414151,
+  //   lat: -16.715799249062005,
+  //   lng: -49.2982311414151,
+  lat: -16.715799,
+  lng: -49.298231,
 }
 
-const googleMapsKeySchema = z.string().min(1)
+const googleMapsEnvSchema = z.object({
+  NEXT_PUBLIC_MAP_ID: z.string().min(1),
+  NEXT_PUBLIC_MAPS_API_KEY: z.string().min(1),
+})
+const envParse = googleMapsEnvSchema.safeParse({
+  NEXT_PUBLIC_MAP_ID: process.env.NEXT_PUBLIC_MAP_ID,
+  NEXT_PUBLIC_MAPS_API_KEY: process.env.NEXT_PUBLIC_MAPS_API_KEY,
+})
+const mapsAPIKey = envParse.data
 
-export default function MapComponent() {
-  const envParse = googleMapsKeySchema.safeParse(
-    process.env.NEXT_PUBLIC_MAPS_API_KEY
+import {
+  APIProvider,
+  Map as MapComponent,
+  useMap,
+  AdvancedMarker,
+  Pin,
+} from '@vis.gl/react-google-maps'
+import { MarkerClusterer } from '@googlemaps/markerclusterer'
+import type { Marker } from '@googlemaps/markerclusterer'
+import { useEffect, useState, useRef } from 'react'
+
+export default function Intro() {
+  if (!mapsAPIKey) {
+    return (
+      <div className="h-80 w-screen">
+        <h1>Invalid Maps API Key</h1>
+        <p>
+          Please check your <code>.env</code> file and make sure you have a
+          valid Google Maps API key.
+        </p>
+      </div>
+    )
+  }
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <APIProvider apiKey={mapsAPIKey.NEXT_PUBLIC_MAPS_API_KEY}>
+        <MapComponent
+          center={{ ...center }}
+          //   zoom={20}
+          defaultZoom={20}
+          mapId={mapsAPIKey.NEXT_PUBLIC_MAP_ID}
+        >
+          <Markers
+            points={[{ lat: center.lat, lng: center.lng, key: 'Sun Light' }]}
+          />
+        </MapComponent>
+      </APIProvider>
+    </div>
   )
-  const isMapsKeyValid = envParse.success
-  const mapsAPIKey = envParse.data ?? 'KEY-NOT-FOUND'
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: mapsAPIKey,
-  })
+}
+
+type Point = google.maps.LatLngLiteral & { key: string }
+type Props = { points: Point[] }
+
+const Markers = ({ points }: Props) => {
+  const map = useMap()
+  const [markers, setMarkers] = useState<{ [key: string]: Marker }>({})
+  const clusterer = useRef<MarkerClusterer | null>(null)
+
+  useEffect(() => {
+    if (!map) return
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({ map })
+    }
+  }, [map])
+
+  useEffect(() => {
+    clusterer.current?.clearMarkers()
+    clusterer.current?.addMarkers(Object.values(markers))
+  }, [markers])
+
+  const setMarkerRef = (marker: Marker | null, key: string) => {
+    if (marker && markers[key]) return
+    if (!marker && !markers[key]) return
+
+    setMarkers(prev => {
+      if (marker) {
+        return { ...prev, [key]: marker }
+      } else {
+        const newMarkers = { ...prev }
+        delete newMarkers[key]
+        return newMarkers
+      }
+    })
+  }
 
   return (
     <>
-      <div className="h-96 w-screen">
-        {isLoaded && isMapsKeyValid ? (
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={17}
-          >
-            <Marker
-              position={center}
-              options={{
-                label: {
-                  text: 'Sun Light',
-                  className: 'map-marker text-green-800',
-                },
-              }}
-              onClick={e => {
-                navigator.clipboard.writeText('')
-              }}
-            />
-          </GoogleMap>
-        ) : (
-          <></>
-        )}
-      </div>
+      {points.map(point => (
+        <AdvancedMarker
+          position={point}
+          key={point.key}
+          ref={marker => setMarkerRef(marker, point.key)}
+        >
+          <Pin />
+        </AdvancedMarker>
+      ))}
     </>
   )
 }
