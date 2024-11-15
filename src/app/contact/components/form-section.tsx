52f9d717d38c0998be8d0b57e00ebdc87cc43689
Caio <caiocpe@gmail.com>
2024-11-15 01:36:57 -0300

    contact page: hoof-form implemented; emailjs supressed

--- src/app/contact/components/form-section.tsx
+++ src/app/contact/components/form-section.tsx
@@ -6,8 +6,10 @@ import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import { Textarea } from '@/components/ui/textarea'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { useEffect } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import { z } from 'zod'
+import emailjs from '@emailjs/browser'
 
 const formSchema = z.object({
   name: z.string(),
@@ -17,10 +19,72 @@ const formSchema = z.object({
   message: z.string(),
   phoneUsesWhatsapp: z.enum(['yes', 'no']),
 })
+type FormData = z.infer<typeof formSchema>
+
+const emailjsEnvSchema = z.object({
+  NEXT_PUBLIC_EMAIL_API_PUBLIC_KEY: z.string().min(1),
+  NEXT_PUBLIC_EMAIL_API_SERVICE_ID: z.string().min(1),
+  NEXT_PUBLIC_EMAIL_API_TEMPLATE_ID: z.string().min(1),
+})
+const envParse = emailjsEnvSchema.safeParse({
+  NEXT_PUBLIC_EMAIL_API_PUBLIC_KEY:
+    process.env.NEXT_PUBLIC_EMAIL_API_PUBLIC_KEY,
+  NEXT_PUBLIC_EMAIL_API_SERVICE_ID:
+    process.env.NEXT_PUBLIC_EMAIL_API_SERVICE_ID,
+  NEXT_PUBLIC_EMAIL_API_TEMPLATE_ID:
+    process.env.NEXT_PUBLIC_EMAIL_API_TEMPLATE_ID,
+})
+const emailjsKey = envParse.data
+
 export default function SectionForm() {
-  const form = useForm({
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: 'text name',
+      email: 'email@email.com',
+      address: 'address 1',
+      phone: '99999999999',
+      message: 'test message',
+      phoneUsesWhatsapp: 'yes',
+    },
   })
+
+  async function handleSubmitFn(data: FormData) {
+    if (!emailjsKey) {
+      throw new Error('EmailJS keys not found')
+    }
+
+    // await new Promise(resolve => setTimeout(resolve, 1000 * 5))
+    console.log(data)
+
+    const params: Record<string, any> = {}
+
+    params.from_name = data.name
+    params.email = data.email
+    params.message = `Dados do cliente:\nEndereço: ${data.address}\nTelefone: ${data.phone}\n`
+    params.message +=
+      data.phoneUsesWhatsapp === 'yes'
+        ? 'este número tambem usa Whatsapp\n'
+        : 'este número não usa Whatsapp\n'
+    params.message += `Mensagem: ${data.message}`
+
+    console.log(params)
+
+    // await emailjs.send(
+    //   emailjsKey.NEXT_PUBLIC_EMAIL_API_SERVICE_ID,
+    //   emailjsKey.NEXT_PUBLIC_EMAIL_API_TEMPLATE_ID,
+    //   params,
+    //   emailjsKey.NEXT_PUBLIC_EMAIL_API_PUBLIC_KEY,
+    // )
+  }
+  useEffect(() => {
+    console.log(errors)
+  }, [errors])
 
   return (
     <>
@@ -46,57 +110,88 @@ export default function SectionForm() {
 
           <div className="flex flex-col gap-3">
             <h4 className="mt-10 mb-3 font-semibold">Envie um email</h4>
-            <form className="lg:w-[700px] xl:w-[1000px]">
+            <form
+              className="lg:w-[700px] xl:w-[1000px]"
+              onSubmit={handleSubmit(handleSubmitFn)}
+            >
               <fieldset className="flex flex-col gap-3 md:grid md:grid-cols-2">
                 <Input
+                  {...register('name')}
                   placeholder="Nome completo"
                   className="placeholder:text-muted-foreground/50"
                 />
                 <Input
+                  {...register('email')}
                   placeholder="Seu email"
                   className="placeholder:text-muted-foreground/50"
                 />
                 <Input
+                  {...register('address')}
                   placeholder="Endereço"
                   className="placeholder:text-muted-foreground/50"
                 />
                 <fieldset>
                   <Input
+                    {...register('phone')}
                     placeholder="Celular"
                     className="placeholder:text-muted-foreground/50"
                   />
-                  <RadioGroup defaultValue="yes" className="inline-flex">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem
-                        value="yes"
-                        id="yes"
-                        className="border-input text-zinc-500"
-                      />
-                      <Label htmlFor="yes" className="text-accent-100/50">
-                        Sim
+                  <Controller
+                    control={control}
+                    name="phoneUsesWhatsapp"
+                    render={({ field }) => (
+                      <Label>
+                        <span className="mr-2 text-xs text-zinc-400">
+                          Este número usa WhatsApp
+                        </span>
+                        <RadioGroup
+                          defaultValue="yes"
+                          className="inline-flex"
+                          onValueChange={field.onChange}
+                          value={field.value}
+                        >
+                          <div className="flex items-center space-x-2">
+                            <RadioGroupItem
+                              value="yes"
+                              id="yes"
+                              className="border-input text-zinc-500"
+                            />
+                            <Label
+                              htmlFor="yes"
+                              className="text-accent-100/50 text-xs"
+                            >
+                              Sim
+                            </Label>
+                          </div>
+                          <div className="flex items-center space-x-2">
+                            <RadioGroupItem
+                              value="no"
+                              id="no"
+                              className="border-input text-zinc-500"
+                            />
+                            <Label
+                              htmlFor="no"
+                              className="text-accent-100/50 text-xs"
+                            >
+                              Não
+                            </Label>
+                          </div>
+                        </RadioGroup>
                       </Label>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem
-                        value="no"
-                        id="no"
-                        className="border-input text-zinc-500"
-                      />
-                      <Label htmlFor="no" className="text-accent-100/50">
-                        Não
-                      </Label>
-                    </div>
-                  </RadioGroup>
+                    )}
+                  />
                 </fieldset>
               </fieldset>
               <fieldset>
                 <Label className="text-zinc-500">
-                  Mensagem <Textarea className="mt-3" />
+                  Mensagem{' '}
+                  <Textarea {...register('message')} className="mt-3" />
                 </Label>
               </fieldset>
               <Button
                 type="submit"
                 className="mt-4 w-fit rounded-full bg-accent-100 px-5"
+                disabled={isSubmitting}
               >
                 Enviar
               </Button>
