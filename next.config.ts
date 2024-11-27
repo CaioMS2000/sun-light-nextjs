2b0ccfa115b30fd57d4cf569cdbf85baea4bc4ed
Caio <caiocpe@gmail.com>
2024-11-27 20:43:09 -0300

    removing unused config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    domains: [
-      "ac0dd60f26eef9386d455730321d63d4.r2.cloudflarestorage.com",
-    ],
-  }
 };
 
 export default nextConfig;
