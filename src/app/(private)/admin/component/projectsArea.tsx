7f8497240f1d115e270bd89c819cb2c102865389
Caio <caiocpe@gmail.com>
2024-11-27 20:53:09 -0300

    avoiding pre-render error on build

--- src/app/(private)/admin/component/projectsArea.tsx
+++ src/app/(private)/admin/component/projectsArea.tsx
@@ -1,11 +1,72 @@
 import { ProcessedProject } from '@/@types/project'
 import ProjectBox from './projectBox'
 import { Hotel } from 'lucide-react'
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { env } from '@/env'
+import { getProjects } from '@/functions/get-projects'
+import { Readable } from 'node:stream'
 
 export default async function ProjectsArea() {
-	const url = new URL('/get-projects', process.env.__NEXT_PRIVATE_ORIGIN)
-	const response = await fetch(url)
-	const data = await response.json()
+	const client = new S3Client({
+		endpoint: `https://${env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+		region: 'auto',
+		credentials: {
+			accessKeyId: env.AWS_ACCESS_KEY_ID!,
+			secretAccessKey: env.AWS_SECRET_ACCESS_KEY!,
+		},
+	})
+
+	async function getBase64ImageFromS3(body: Readable, mimeType: string) {
+		const chunks: Buffer[] = []
+
+		for await (const chunk of body) {
+			chunks.push(Buffer.from(chunk))
+		}
+
+		const buffer = Buffer.concat(chunks)
+		return `data:${mimeType};base64,${buffer.toString('base64')}`
+	}
+
+	const allProjects = await getProjects()
+	const processedProjects: ProcessedProject[] = []
+
+	for (const project of allProjects) {
+		const base64Images = []
+		for (const imageURL of project.imageURLs) {
+			const command = new GetObjectCommand({
+				Bucket: env.AWS_BUCKET_NAME,
+				Key: imageURL,
+			})
+			const response = await client.send(command)
+
+			if (!response.Body) {
+				throw new Error('File not found')
+			}
+
+			const mimeType = response.ContentType
+
+			if (!mimeType) {
+				throw new Error('Content type not found')
+			}
+
+			const base64Image = await getBase64ImageFromS3(
+				response.Body as Readable,
+				mimeType
+			)
+
+			base64Images.push(base64Image)
+		}
+
+		processedProjects.push({
+			id: project.id,
+			name: project.name,
+			address: project.address,
+			potency: project.potency,
+			estimation: project.estimation,
+			pj: project.pj,
+			images: base64Images,
+		})
+	}
 
 	return (
 		<>
@@ -14,7 +75,7 @@ export default async function ProjectsArea() {
 				Projetos
 			</h3>
 			<div className="flex flex-col gap-6">
-				{data.projects?.map((project: ProcessedProject) => (
+				{processedProjects.map((project: ProcessedProject) => (
 					<ProjectBox key={project.id} {...project} />
 				))}
 			</div>
--- src/app/(private)/admin/page.tsx
+++ src/app/(private)/admin/page.tsx
@@ -2,6 +2,7 @@ import UploadForm from './component/uploadForm'
 import { Separator } from '@/components/ui/separator'
 import ProjectsArea from './component/projectsArea'
 import Header from './header'
+import { Suspense } from 'react'
 
 export default function Admin() {
 	return (
@@ -9,7 +10,9 @@ export default function Admin() {
 			<Header />
 			<UploadForm />
 			<Separator />
-			<ProjectsArea />
+			<Suspense fallback={<div>Loading...</div>}>
+				<ProjectsArea />
+			</Suspense>
 
 			<div id="white-space" className="h-14 bg-transparent"></div>
 		</>
