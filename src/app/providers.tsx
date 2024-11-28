7b1b7b20265d346bdfde84fa1d82ffc04705396e
Caio <caiocpe@gmail.com>
2024-11-27 23:21:53 -0300

    react query setup

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { KeyPressProvider } from '@/context/keyPressContext'
 import { queryClient } from '@/lib/react-query'
 import { QueryClientProvider } from '@tanstack/react-query'
