a25501348fc25411e4c18c9b7528739a7eadef5e
Caio <caiocpe@gmail.com>
2024-11-27 23:13:57 -0300

    react query setup

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,13 @@
 import { KeyPressProvider } from '@/context/keyPressContext'
+import { queryClient } from '@/lib/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<>
-			<KeyPressProvider>{children}</KeyPressProvider>
+			<KeyPressProvider>
+				<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+			</KeyPressProvider>
 		</>
 	)
 }
