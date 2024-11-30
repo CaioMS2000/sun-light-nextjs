988dee6d77084d1a0a12314b0880052ad4bd1ab9
Caio <caiocpe@gmail.com>
2024-11-30 14:47:52 -0300

    whatsApp button

--- src/components/whats-app-button.tsx
+++ src/components/whats-app-button.tsx
@@ -1,22 +1,37 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/components/ui/tooltip'
 
 export default function WhatsAppButton() {
 	return (
 		<>
-			<Link
-				href={'https://api.whatsapp.com/send?phone=556232897574'}
-				target="_blank"
-			>
-				<Image
-					src={'https://cdn-icons-png.flaticon.com/512/4494/4494494.png'}
-					alt=""
-					width="0"
-					height="0"
-					sizes="100px"
-					className="fixed right-10 bottom-10 z-50 w-12 cursor-pointer rounded-full lg:w-16"
-				/>
-			</Link>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger>
+						<Link
+							href={'https://api.whatsapp.com/send?phone=556232897574'}
+							target="_blank"
+						>
+							<Image
+								src={'https://cdn-icons-png.flaticon.com/512/4494/4494494.png'}
+								alt=""
+								width="0"
+								height="0"
+								sizes="100px"
+								className="fixed right-10 bottom-10 z-50 w-12 cursor-pointer rounded-full lg:w-16"
+							/>
+						</Link>
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>Entre em contato no WhatsApp</p>
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
 		</>
 	)
 }
