9b44d4a6278ec490983d1881fa44d201d1ee888e
Caio <caiocpe@gmail.com>
2024-11-30 03:33:18 -0300

    caching nextjs images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
+      minimumCacheTTL: 60 * 1 * 60 * 24,
     remotePatterns: [
       {
         protocol: 'https',
