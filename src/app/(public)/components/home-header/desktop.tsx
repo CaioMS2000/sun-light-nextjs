3964268e21c75bb36ce7eea48829bad29ac512a3
Caio <caiocpe@gmail.com>
2024-12-02 17:02:36 -0300

    restyling homeheader

--- src/app/(public)/components/home-header/desktop.tsx
+++ src/app/(public)/components/home-header/desktop.tsx
@@ -14,7 +14,7 @@ export default function DesktopHomeHeader() {
 		<>
 			<header className="relative hidden h-[400px] w-screen md:flex">
 				<NavHeader className="absolute w-screen text-white" />
-				<div className="flex h-full flex-1 items-end bg-accent-100 pb-8 text-white">
+				<div className="flex h-full max-w-[410px] items-end bg-accent-100 pb-8 text-white">
 					<div className="space-y-10 pl-14">
 						<h3 className="text-4xl">Redefinindo Energia</h3>
 						<p className="max-w-80 text-wrap text-xs">
@@ -29,7 +29,7 @@ export default function DesktopHomeHeader() {
 						</Button>
 					</div>
 				</div>
-				<div className='flex h-full w-[60%] items-end bg-[url("/images/banners/IMG_8447.png")] bg-cover'></div>
+				<div className='flex h-full w-[60%] flex-1 items-end bg-[url("/images/banners/IMG_8447.png")] bg-cover xl:bg-[position:0%_10%] 2xl:bg-[position:0%_20%]'></div>
 			</header>
 		</>
 	)
