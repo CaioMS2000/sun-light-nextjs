eac34435cf3155ae8435532b99dc1d1f8ae88662
Caio <caiocpe@gmail.com>
2024-12-02 22:30:29 -0300

    delay before redirect

--- src/app/(private)/sign-in/page.tsx
+++ src/app/(private)/sign-in/page.tsx
@@ -61,6 +61,7 @@ function SignIn() {
 			}
 
 			if (response.redirected && response.url) {
+				await new Promise(resolve => setTimeout(resolve, 500))
 				router.push(response.url)
 			}
 		} catch (error) {
